904d428b668fa61d64d60466bdbad85596406e79
BenDev181 <benjiro181@gmail.com>
2024-10-15 11:19:46 +0100

    day2 tasks complete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,30 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+const { where } = require("sequelize");
 
-//TODO: Create your GET Request Route Below: 
-app.get("/restaurants", async (request, response) => {
-    let restaurants = await Restaurant.findAll()
-    response.json(restaurants)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/restaurants/:id", async (req, res) => {
+    const id = req.params.id
+    let restaurant = await Restaurant.findByPk(id)
+    res.json(restaurant)
+})
+
+app.post("/restaurants", async (req, res) => {
+    const restaurant = await Restaurant.create(req.body)
+    res.json(restaurant)
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    const updatedRestaurant = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedRestaurant)
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+    const deletedRestaurant = await Restaurant.destroy({where: {id: req.params.id}})
+    res.json(deletedRestaurant)
 })
 
 
